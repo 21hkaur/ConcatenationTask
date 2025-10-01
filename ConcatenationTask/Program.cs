5c2216b4cd2a7a703e7b6f7b6bbdd353d9f600cb
Harlean Kaur <21HKaur@heckgrammar.co.uk>
2025-10-01 10:09:37 +0100

    commit 3

--- ConcatenationTask/Program.cs
+++ ConcatenationTask/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConcatenationTask
+﻿using System.Xml.Linq;
+
+namespace ConcatenationTask
 {
     internal class Program
     {
@@ -12,7 +14,13 @@
             // use the test data Bob, Smith, 14
             // ammend this code to output in one command
             // Hello Bob Smith you are 14 years old
-            Console.WriteLine(FirstName);
+            string LastName;
+            Console.Write("Enter last name: ");
+            LastName = Console.ReadLine();
+            int age;
+            Console.Write("Enter age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello " + FirstName + " " + LastName + " your are " + age + " years old");
             // save and commit your program
             // add evidence of the test input and your own input running the program
         }
